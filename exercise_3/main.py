48168e95fa3f106b92a7fc81b882fad0c37ee18c
Yoga Pratama Aliarham <yoga.aliarham@tiket.com>
2021-03-15 11:37:18 +0700

    Fix UserRegistration object

--- exercise_3/main.py
+++ exercise_3/main.py
@@ -12,12 +12,14 @@ class UserRegistration(object):
 			user_id = str(uuid4())
 			self.users[user_id] = user
 			self.logger.info("user {} has successfully created".format(user["name"]))
+			return user_id
 		except Exception as e:
 			self.logger.error("cannot create user")
+			return None
 	
 	def delete_user(self, user_id):
 		try:
 			del self.users[user_id]
-			self.logger.info("user_id {} has successfully created".format(user_id))
+			self.logger.info("user_id {} has successfully deleted".format(user_id))
 		except Exception as e:
 			self.logger.error("cannot delete user")
